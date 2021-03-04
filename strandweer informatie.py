182b14bc682087e4bb932bba72d9bd29419451c4
Sander van Vliet <a.p.van.vliet@hva.nl>
2021-03-04 22:43:54 +0100

    api key vervangen met api key die hourly forecast geeft
    api key vervangen met api key die hourly forecast geeft en alles met max_index en max_index_str even outcommented omdat het nu met deze api key data niet meer werkt. Moet nog opgelost worden.

--- strandweer informatie.py
+++ strandweer informatie.py
@@ -10,6 +10,8 @@ content = my_file.read()
 lijst_met_plaatsnamen = content.split(",")
 # bestand plaatsnamen.txt wordt gesloten
 my_file.close()
+# list variabele genaamd "lijst_met_plaatsnamen" wordt geprint
+##print(lijst_met_plaatsnamen)
 
 import inquirer
 
@@ -26,23 +28,20 @@ q = answers["Plaatsnaam_keuze"]
 import requests
 import json
 
-appid = "a7f9e1a6b04a8b787e857547840c3a36"
-url = (
-    "https://api.openweathermap.org/data/2.5/forecast?q=%s&appid=%s&units=metric&cnt=5"
-    % (
-        q,
-        appid,
-    )
+appid = "8f3a5f240b1131a25d95fbb2c6e30275"
+url = "https://pro.openweathermap.org/data/2.5/forecast/climate?q=%s&appid=%s&units=metric" % (
+    q,
+    appid,
 )
 
 response = requests.get(url)
 data = json.loads(response.text)
 
-temp0 = data["list"][0]["main"]["feels_like"]
-temp1 = data["list"][1]["main"]["feels_like"]
-temp2 = data["list"][2]["main"]["feels_like"]
-temp3 = data["list"][3]["main"]["feels_like"]
-temp4 = data["list"][4]["main"]["feels_like"]
+temp0 = data["list"][0]["feels_like"]["day"]
+temp1 = data["list"][1]["feels_like"]["day"]
+temp2 = data["list"][2]["feels_like"]["day"]
+temp3 = data["list"][3]["feels_like"]["day"]
+temp4 = data["list"][4]["feels_like"]["day"]
 
 ##print(("De hoogste temperatuur is:"), (max(temp0, temp1, temp2, temp3, temp4),))
 
@@ -54,7 +53,7 @@ degree_sign = u"\N{DEGREE SIGN}"
 
 # Print onderstaande
 print(
-    ("De verwachte temperaturen voor de komende vijf dagen zijn:"),
+    ("De gevoelstemperaturen verwacht om 12u voor de komende vijf dagen zijn:"),
     (temp0, temp1, temp2, temp3, temp4),
 )
 
@@ -73,43 +72,43 @@ lijst_temp_van_5_dagen = [temp0, temp1, temp2, temp3, temp4]
 # Zie https://www.kite.com/python/answers/how-to-find-the-index-of-the-max-value-in-a-list-in-python
 # The max value in a list is the element with the highest value. Finding the index of the max value in
 # a list returns the position of the max value within the list.
-max_index = lijst_temp_van_5_dagen.index(maxtemp)
+##max_index = lijst_temp_van_5_dagen.index(maxtemp)
 
 # Print ter controle het indexnummer
-print(
-    ("het index nummer van de positie van de hoogste waarde in de lijst is:"),
-    (max_index),
-)
+##print(
+##("het index nummer van de positie van de hoogste waarde in de lijst is:"),
+##(max_index),
+##)
 # Omdat de lijst begint met element 0
 # Maak integer variabele aan die de waarde heeft van het index nr van de dag met de hoogste temp verhoogd met 1
-dag_met_max_temp = max_index + 1
+##dag_met_max_temp = max_index + 1
 
 # Converteer de integer variabele naar een string variabele (change variable type), anders kan dit niet
 # omdat het twee verschillende variable types zijn, namelijk string en integer: max_index_str + "e dag")
 # Zie https://www.geeksforgeeks.org/convert-integer-to-string-in-python/
-max_index_str = str(dag_met_max_temp)
+##max_index_str = str(dag_met_max_temp)
 
 # Print ter controle de string variabele
-print(("Dit is positie"), (max_index_str))
+##print(("Dit is positie"), (max_index_str))
 
 # Print onderstaande
-print(("Dit is dus op de"), (max_index_str + "e dag"))
+##print(("Dit is dus op de"), (max_index_str + "e dag"))
 
 # Print onderstaande
-print(("De datum van deze dag is:"), (data["list"][max_index]["dt_txt"]))
+##print(("De datum van deze dag is:"), (data["list"][max_index]["dt_txt"]))
 ##print(datum_dag_max_temp)
 
-datum0 = data["list"][0]["dt_txt"]
-datum1 = data["list"][1]["dt_txt"]
-datum2 = data["list"][2]["dt_txt"]
-datum3 = data["list"][3]["dt_txt"]
-datum4 = data["list"][4]["dt_txt"]
+##datum0 = data["list"][0]["dt_txt"]
+##datum1 = data["list"][1]["dt_txt"]
+##datum2 = data["list"][2]["dt_txt"]
+##datum3 = data["list"][3]["dt_txt"]
+##datum4 = data["list"][4]["dt_txt"]
 
-print(datum0)
-print(datum1)
-print(datum2)
-print(datum3)
-print(datum4)
+##print(datum0)
+##print(datum1)
+##print(datum2)
+##print(datum3)
+##print(datum4)
 
 ##print(data["list"][0]["dt_txt"])
 ##print(data["list"][1]["dt_txt"])
